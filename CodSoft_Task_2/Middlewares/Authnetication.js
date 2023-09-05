7b6ea6865fde9c93e7fc3e245994ff623c86c1aa
Kimo <mk5391130@gmail.com>
2023-09-05 20:24:29 +0300

    Final

---
+++ CodSoft_Task_2/Middlewares/Authnetication.js
@@ -0,0 +1,34 @@
+const  jwt = require("jsonwebtoken")
+
+
+const decode  =  require("jwt-decode")
+const { RefreshToken } = require("../Models/index.js")
+
+require("dotenv").config({path:"./Config/.env"})
+
+console.log(process.env.SECRET_KEY,process.env.EXP_PER)
+
+let Authenticate = (req,res,next)=>{
+   
+   let {access_token,refresh_token,email} = req.cookies
+   req.token = access_token;
+   
+
+    if(!access_token || !refresh_token || !email) return res.status(403).json({err:["Invalid"]})
+  
+    jwt.verify(access_token,process.env.SECRET_KEY,(err,decoded)=>{
+        if(err){
+          RefreshToken.find({token:refresh_token}).then(result=> {
+            if(result){
+             const token = jwt.sign({Email:email},process.env.SECRET_KEY,{expiresIn:process.env.EXP_PER});
+             req.token=token;
+            }
+          })
+        }
+        return next();
+       
+    });
+
+}
+
+module.exports =  Authenticate;
